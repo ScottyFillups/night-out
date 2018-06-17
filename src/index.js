1d623bb964dd8aaca8f8d86510a0242ffd69ae3d
Philip Scott <pscott@zeptohost.com>
2018-06-17 13:23:09 -0400

    feat(music): Add music

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello, world')
+import songUrl from './song.mp3'
+import axios from 'axios'
+
+const $ = (slt) => document.querySelector(slt)
+
+const audioCtx = new AudioContext()
+const source = audioCtx.createBufferSource()
+const req = {
+  responseType: 'arraybuffer',
+  url: songUrl,
+  method: 'get'
+}
+
+axios.request(req).then((res) => {
+  audioCtx.decodeAudioData(res.data).then((buffer) => {
+    source.buffer = buffer
+    source.connect(audioCtx.destination)
+    source.start()
+  })
+})
