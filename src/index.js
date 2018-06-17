4a6a0342ded45c765a4c72658c5c9331b09a720b
Philip Scott <pscott@zeptohost.com>
2018-06-17 13:57:12 -0400

    feat(viz): IT WORKS! :3

--- src/index.js
+++ src/index.js
@@ -5,16 +5,29 @@ const $ = (slt) => document.querySelector(slt)
 
 const audioCtx = new AudioContext()
 const source = audioCtx.createBufferSource()
-const req = {
-  responseType: 'arraybuffer',
-  url: songUrl,
-  method: 'get'
-}
+const analyser = audioCtx.createAnalyser()
+const vizData = new Uint8Array(analyser.frequencyBinCount)
+const $viz = $('#viz')
 
-axios.request(req).then((res) => {
+analyser.fftSize = 2048
+
+axios.get(songUrl, { responseType: 'arraybuffer' }).then((res) => {
   audioCtx.decodeAudioData(res.data).then((buffer) => {
     source.buffer = buffer
+    source.connect(analyser)
     source.connect(audioCtx.destination)
     source.start()
   })
-})
+}).catch(err => console.error('ERROR: ', err))
+
+function animViz () {
+  requestAnimationFrame(animViz)
+
+  analyser.getByteTimeDomainData(vizData)
+
+  const factor = vizData[0] / 100
+
+  $viz.style.transform = `translate(-50%, -50%) scaleY(${factor}) scaleX(${factor})`
+}
+
+animViz()
