3cc9af41537c1ab009a58e7209253ec10cafd901
Philip Scott <pscott@zeptohost.com>
2018-06-17 14:51:12 -0400

    feat(button): Add play button (#5)

--- src/index.js
+++ src/index.js
@@ -3,33 +3,40 @@ import axios from 'axios'
 
 const $ = (slt) => document.querySelector(slt)
 
-const audioCtx = new AudioContext()
-const source = audioCtx.createBufferSource()
-const analyser = audioCtx.createAnalyser()
-const vizData = new Uint8Array(analyser.frequencyBinCount)
+let analyser
+let vizData
 const $viz = $('#viz')
+const $play = $('#play')
 
-analyser.fftSize = 2048
+function play () {
+  const audioCtx = new AudioContext()
+  const source = audioCtx.createBufferSource()
 
-axios.get(songUrl, { responseType: 'arraybuffer' }).then((res) => {
-  audioCtx.decodeAudioData(res.data).then((buffer) => {
-    source.buffer = buffer
-    source.connect(analyser)
-    source.connect(audioCtx.destination)
-    source.start()
-  })
-}).catch(err => console.error('ERROR: ', err))
+  analyser = audioCtx.createAnalyser()
+  analyser.fftSize = 2048
+  vizData = new Uint8Array(analyser.frequencyBinCount)
+  $play.style.opacity = 0
+  $play.style.cursor = "default"
 
-function lerp (a, b, x) {
-  return a + (b - a) * x
+  axios.get(songUrl, { responseType: 'arraybuffer' }).then((res) => {
+    audioCtx.decodeAudioData(res.data).then((buffer) => {
+      source.buffer = buffer
+      source.loop = true
+      source.connect(analyser)
+      source.connect(audioCtx.destination)
+      source.start()
+    })
+  }).catch(err => console.error('ERROR: ', err))
+
+  setInterval(animViz, 100)
 }
 
 function animViz () {
   analyser.getByteTimeDomainData(vizData)
 
-  const factor = vizData[0] / 100
+  const factor = vizData[0] / 130
 
   $viz.style.transform = `translate(-50%, -50%) scaleY(${factor}) scaleX(${factor})`
 }
 
-setInterval(animViz, 100)
+$play.addEventListener('click', play)
